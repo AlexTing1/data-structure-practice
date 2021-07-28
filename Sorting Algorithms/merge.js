2f4d99d80cf8a143a19af190712bb316467753cf
Alex Ting <alexting6366@gmail.com>
2021-07-28 09:23:50 -0700

    finished mergesort algo

--- Sorting Algorithms/merge.js
+++ Sorting Algorithms/merge.js
@@ -1,11 +1,26 @@
 var merge = function(input) {
-  if (input === []) {
-    return [];
-  };
-
-  var result = [];
-  var helper = function(currentArray) {
-    var mid = Math.floor(currentArray.length/2) + 1
-
+  if (input.length <= 1){
+    return input;
   }
+
+  var mid = Math.floor(input.length / 2);
+  var left = input.slice(0, mid);
+  var right = input.slice(mid);
+
+  return mergeHelper(merge(left), merge(right));
 }
+
+var mergeHelper = function(left, right) {
+  const array = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      array.push(left.shift());
+    } else {
+      array.push(right.shift());
+    }
+  }
+
+  return array.concat(left.slice()).concat(right.slice())
+}
+
