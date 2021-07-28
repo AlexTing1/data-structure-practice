33b3dc20029d3efe4fb6aa253d65447a87cf147a
Alex Ting <alexting6366@gmail.com>
2021-07-28 09:00:42 -0700

    Started Merge sort algo

---
+++ Sorting Algorithms/merge.js
@@ -0,0 +1,11 @@
+var merge = function(input) {
+  if (input === []) {
+    return [];
+  };
+
+  var result = [];
+  var helper = function(currentArray) {
+    var mid = Math.floor(currentArray.length/2) + 1
+
+  }
+}
