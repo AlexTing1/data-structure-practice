aa61d2b0ff7750bd0d78679290ab38553fad791e
Alex Ting <alexting6366@gmail.com>
2021-08-01 23:03:39 -0700

    completed smallestDifference.js

---
+++ Algo Expert/smallestDifference.js
@@ -0,0 +1,29 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+	arrayOne.sort(function(a, b) {return a - b});
+	arrayTwo.sort(function(a, b) {return a - b});
+
+	var countOne = 0;
+	var countTwo = 0;
+	var result = [];
+	var smallestAbs = Infinity;
+	while (countOne < arrayOne.length && countTwo < arrayTwo.length) {
+		var currentOne = arrayOne[countOne];
+		var currentTwo = arrayTwo[countTwo];
+		var difference = Math.abs(currentOne - currentTwo);
+		if (difference < smallestAbs) {
+			smallestAbs = difference;
+			result = [currentOne, currentTwo];
+		}
+
+		if (currentOne < currentTwo) {
+			countOne++;
+		} else {
+			countTwo++;
+		}
+	}
+	return result;
+}
+
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
