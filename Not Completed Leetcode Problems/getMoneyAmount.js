a4fcd27eac21f94d531be4b60d75d43757d6d71d
Alex Ting <alexting6366@gmail.com>
2021-08-11 00:13:35 -0700

    started minDifference.js

--- Not Completed Leetcode Problems/getMoneyAmount.js
+++ Not Completed Leetcode Problems/getMoneyAmount.js
@@ -4,5 +4,35 @@
 //  Edge Cases - n = 1, n = 2
 
 var getMoneyAmount = function(n) {
+  //  init trackerObj
+  //  init range
+  //  create helper function to iterate
+  //  do Binary search on every number and record what payout will be,
+  //    if new payout is greater than current payout, then replace that with current payout
 
+  var largestPayout = 0;
+  var tracker = [];
+  var guess = ;
+  var helper = function(min, max, currentPayout, prevPayout, guess) {
+    var mid =  Math.floor((min + max)/2);
+    var upperMid = Math.floor((guess + max)/2);
+    var lowerMid = Math.floor((guess + min)/2);
+    currentPayout += prevPayout;
+    if (currentPayout > largestPayout) {
+      largestPayout = currentPayout;
+    }
+
+    if (min === max) {
+      return;
+    }
+    //go right
+    helper(guess + 1, max, currentPayout, guess, upperMid);
+    //go left
+    helper(min, guess, currentPayout, guess, lowerMid);
+  }
+
+  helper(1, n, 0, 0, guess);
+  return largestPayout;
 };
+
+console.log(getMoneyAmount(10));
