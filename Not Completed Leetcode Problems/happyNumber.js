cbc64ecb1f1fcabf6a7f2fcbd9adf3a773ad417e
Alex Ting <alexting6366@gmail.com>
2021-08-12 22:13:05 -0700

    started happyNumber.js

---
+++ Not Completed Leetcode Problems/happyNumber.js
@@ -0,0 +1,13 @@
+var isHappy = function(n) {
+  var trackerObj = {};
+
+  var helper = function(n) {
+      if (n === 1) {
+          return true;
+      }
+
+      if (trackerObj[n] !== undefined) {
+
+      }
+  }
+};
--- Not Completed Leetcode Problems/numDecodings.js
+++ Not Completed Leetcode Problems/numDecodings.js
@@ -14,16 +14,32 @@ var numDecodings = function(s) {
   //      else continue
 
   var count = 0;
+  var dictionary = ['1', '2', '3', '4', '5', '6', '7' ,'8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
   var helper = function(string) {
+    if (string === undefined) {
+      return;
+    }
     if (string.length === 0) {
       count++;
       return;
     }
 
-    var oneDigit = parseInt(string[0]);
-    var twoDigit = parseInt(string[0] + string[1]);
+    var oneDigit = string[0];
+    var twoDigit = string[0] + string[1];
 
+    if (dictionary.includes(oneDigit)) {
+      helper(string.slice(1))
+    }
+
+    if (dictionary.includes(twoDigit)) {
+      helper(string.slice(2));
+    }
+
+    return;
   }
+
+  helper(s);
+  return count;
 };
 
-console.log(parseInt('01'));
+console.log(numDecodings("0"));
