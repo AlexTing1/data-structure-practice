c3962f533ab2eab3ca91afe86da67ed6609a9dc6
Alex Ting <alexting6366@gmail.com>
2021-08-12 17:15:27 -0700

    completed mergeIntevals.js

--- Not Completed Leetcode Problems/mergeIntervals.js
+++ Not Completed Leetcode Problems/mergeIntervals.js
@@ -3,21 +3,37 @@
 //  contraints - 1 <= intervals.length <= 10^4, intervals[i].length == 2, 0 <= starti <= endi <= 104
 //  Edge cases - no interavl overlap, 1 interval overlap, interval overlap happens in front, interval overlap happens in back, same intervals
 var merge = function(intervals) {
-  //  init result array
+  //  sort intervals
+  //  init results array
   //  iterate intervals
-  //    iterate results array
-  //      if interavls falls within results array
-  //        change current interval in results array
-  //      else
-  //        add new interval to results array
-  //  return results array
+  //    if current Intervals is within last result, then do nothing
+  //    if first interval of current > last interval of result
+  //      push into result
+  //    change approrpityl
+  //  return result
 
-  var results = [];
+  intervals.sort(function(a, b) { return a[0] - b[0] });
+  debugger;
+  var results = [intervals[0]];
+  for (var i = 1; i < intervals.length; i++) {
+    var lastResult = results[results.length - 1];
+    var currentInterval = intervals[i];
 
-  for (var i = 0; i < intervals.length; i++) {
-    var isIn = false;
-    for (var j = 0; j < results.length; j++) {
-
+    if (currentInterval[0] >= lastResult[0] && currentInterval[0] <= lastResult[1] && currentInterval[1] >= lastResult[0] && currentInterval[1] <= lastResult[1]) {
+      continue;
+    } else if (currentInterval[0] > lastResult[1]) {
+      results.push(currentInterval);
+    } else {
+      results[results.length - 1][1] = currentInterval[1];
     }
+
   }
+  debugger;
+  return results;
 }
+
+var test = [[1,3],[2,6],[8,10],[15,18]];
+var test2 =
+[[1,3],[2,6],[8,10],[15,18]];
+
+console.log(merge(test2));
