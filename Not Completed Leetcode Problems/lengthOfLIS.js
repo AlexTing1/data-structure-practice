61ffb50a9c21129c3611adccf4865308efcc1c03
Alex Ting <alexting6366@gmail.com>
2021-08-14 11:16:19 -0700

    started majorityElement.js

--- Not Completed Leetcode Problems/lengthOfLIS.js
+++ Not Completed Leetcode Problems/lengthOfLIS.js
@@ -1,3 +1,12 @@
 var lengthOfLIS = function(nums) {
+  var trackerArray = [[nums[0]]];
 
+
+  debugger;
+  return trackerArray
 };
+
+
+var nums = [10,9,2,5,3,7,101,18];
+
+console.log(lengthOfLIS(nums))
