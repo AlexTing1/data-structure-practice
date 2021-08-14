36d9a64385dea1c547bf392e762e8a83da79d7f4
Alex Ting <alexting6366@gmail.com>
2021-08-14 11:22:08 -0700

    completed majorityELemnet.js

--- Not Completed Leetcode Problems/majorityElement.js
+++ Not Completed Leetcode Problems/majorityElement.js
@@ -1,3 +1,33 @@
 var majorityElement = function(nums) {
+  //  init nums.length/2
+  //  init trackerObj
+  //  iterate nums
+  //    if nums[i] not in trackerObj
+  //      trackerObj[nums[i]] = 1;
+  //    else
+  //      tracker[nums[i]]++;
+  //      if tracker[nums[i]] > nums.length/2
+  //        return nums[i]
 
+  var majorElementBar = nums.length/2;
+  var trackerObj = {};
+
+  for (var i = 0; i < nums.length; i++) {
+    var current = nums[i];
+    if (trackerObj[current] === undefined) {
+      trackerObj[current] = 1;
+    } else {
+      trackerObj[current]++;
+    }
+
+    if (trackerObj[nums[i]] > majorElementBar) {
+      return nums[i];
+    }
+  }
+
+  return null;
 };
+
+var test = [3, 2, 3]
+var test2 = [2,2,1,1,1,2,2]
+console.log(majorityElement(test2));
