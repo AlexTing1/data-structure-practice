690b89264bb5fb11fe8914b4eab8226977110c80
Alex Ting <alexting6366@gmail.com>
2021-08-22 12:04:16 -0700

    started combinationSum.js

--- Not Completed Leetcode Problems/singleNumber2.js
+++ Not Completed Leetcode Problems/singleNumber2.js
@@ -1,3 +1,15 @@
 var singleNumber = function(nums) {
+  var result = nums[0];
+
+  for (var i = 1; i < nums.length; i++) {
+    result  = result ^ nums[i];
+  }
+
+  return result;
 
 }
+
+var test = [0,1,0,1,0,1,99];
+
+//console.log(3^2)
+console.log(singleNumber(test));
