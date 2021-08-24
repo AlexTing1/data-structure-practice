5fca677144646b96d0a4869ee0c23330c0562f27
Alex Ting <alexting6366@gmail.com>
2021-08-23 21:50:54 -0700

    completed matrixQuery.js

--- Completed Leetcode Problems/matrixQuery.js
+++ Completed Leetcode Problems/matrixQuery.js
@@ -3,13 +3,14 @@ var matrixQuery = function(n, m, queries) {
   var cols = fillArray(n);
   var result = [];
   for (var i = 0; i < queries.length; i++) {
+    debugger;
     var current = queries[i];
     if (current[0] === 0) {
       result.push(cols[0] * rows[0]);
     } else if (current[0] === 1) {
-      rows = rows.splice(current[1] - 1, 1);
+      rows = rows.slice(0, current[1] - 1).concat(rows.slice(current[1]));
     } else {
-      cols = cols.splice(current[1] - 1, 1);
+      cols = cols.slice(0, current[1] - 1).concat(cols.slice(current[1]));
     }
   }
   return result;
@@ -25,3 +26,4 @@ var fillArray = function(n) {
   return result;
 }
 
+console.log(matrixQuery(3, 4, [[0], [1, 2], [0], [2, 1], [0], [1, 1], [0]]))
