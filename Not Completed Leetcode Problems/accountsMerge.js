a7c01c11de09ec96243f8156327962494f977f79
Alex Ting <alexting6366@gmail.com>
2021-09-05 10:54:02 -0700

    finishe calculate

--- Not Completed Leetcode Problems/accountsMerge.js
+++ Not Completed Leetcode Problems/accountsMerge.js
@@ -1,3 +1,27 @@
 var accountsMerge = function(accounts) {
+  var trackerObj = {};
 
+  for (var i = 0; i < accounts.length; i++) {
+    var current = accounts[i];
+    var name = current[0];
+
+    if (trackerObj[name] === undefined) {
+      //  format email = 1;?
+      var email = {};
+      for (var i = 1; i < current.length; i++) {
+        email[current[i]] = 1;
+      }
+      trackerObj[name] = [email];
+    } else {
+      var users = trackerObj[name];
+      for (var i = 0; i < users.length; i++) {
+        var currentEmailObj = users[i];
+        for (var j = 1; j < current.length; j++) {
+          if (currentEmailObj[current[j]] !== undefined) {
+            //add everything to emailboj
+          }
+        }
+      }
+    }
+  }
 };
--- Not Completed Leetcode Problems/calculate.js
+++ Not Completed Leetcode Problems/calculate.js
@@ -1,3 +1,48 @@
 var calculate = function(s) {
+  s = s.split(' ').join('');
+  var stack = [];
+  var result = 0;
+  var currentNumber = '';
+  var prevSign = '+';
+  for (var i = 0; i <= s.length; i++) {
+    var current = s[i];
+    debugger;
+    if (!isNaN(parseInt(current))) {
+      currentNumber += current;
+    } else {
+      var num = Number(currentNumber);
+      if (prevSign === '+') {
+        stack.push(num);
 
+      }
+
+      if (prevSign === '-') {
+        stack.push(-num);
+
+      }
+
+      if (prevSign === '*') {
+        stack.push(stack.pop()*num)
+      }
+
+      if (prevSign === '/') {
+        stack.push(parseInt(stack.pop()/num, 10))
+      }
+
+      prevSign = current;
+      currentNumber = '';
+    }
+  }
+
+  debugger;
+
+  while (stack.length !== 0) {
+    var num = stack.pop();
+    result += parseInt(num);
+  }
+
+  return result;
 };
+
+var string = "3+2*2"
+console.log(calculate(string));
