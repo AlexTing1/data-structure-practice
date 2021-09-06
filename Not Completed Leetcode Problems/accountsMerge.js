49505f5e0cf9b5ac6fc2eadbb4beed94534eabfb
Alex Ting <alexting6366@gmail.com>
2021-09-05 18:23:07 -0700

    started subarraySum.js

--- Not Completed Leetcode Problems/accountsMerge.js
+++ Not Completed Leetcode Problems/accountsMerge.js
@@ -1,27 +1,74 @@
 var accountsMerge = function(accounts) {
-  var trackerObj = {};
+  var result = [];
+  var nameObj = {};
+  //iterate accounts
+  //  if name does not exists
+  //    add directly to result
+  //  else
+  //    check nameObj if an email exists
+  //    if email exists, then add to email
+  //    else add to result, add to nameObj
+  //return result
+  var createEmailObj = function(emailArray, index) {
+    var result = {};
+    for (var m = 1; m < emailArray.length; m++) {
+      var current = emailArray[m];
+      result[current] = index;
+    }
+    return result;
+  }
 
   for (var i = 0; i < accounts.length; i++) {
-    var current = accounts[i];
-    var name = current[0];
-
-    if (trackerObj[name] === undefined) {
-      //  format email = 1;?
-      var email = {};
-      for (var i = 1; i < current.length; i++) {
-        email[current[i]] = 1;
-      }
-      trackerObj[name] = [email];
+    var currentAccount = accounts[i];
+    var currentName = currentAccount[0];
+    //debugger;
+    if (nameObj[currentName] === undefined) {
+      var emailObj = createEmailObj(currentAccount, result.length - 1);
+      nameObj[currentName] = [emailObj];
     } else {
-      var users = trackerObj[name];
-      for (var i = 0; i < users.length; i++) {
-        var currentEmailObj = users[i];
-        for (var j = 1; j < current.length; j++) {
-          if (currentEmailObj[current[j]] !== undefined) {
-            //add everything to emailboj
+      var users = nameObj[currentName];
+      var toAdd = true;
+      for (var j = 0; j < users.length; j++) {
+        var currentUser = users[j];
+        toAdd = true;
+        for (var k = 1; k < currentAccount.length; k++) {
+          var currentEmail = currentAccount[k];
+          if (currentUser[currentEmail] !== undefined) {
+            for (var l = 1; l < currentAccount.length; l++) {
+              currentUser[currentAccount[l]] = 1;
+            }
+            toAdd = false;
+            break;
           }
         }
+        if (!toAdd) {
+          break;
+        }
+      }
+      if (toAdd) {
+        var emailObj = createEmailObj(currentAccount, result.length - 1);
+        nameObj[currentName].push(emailObj);
       }
     }
+
   }
+  debugger;
+
+  var keys = Object.keys(nameObj);
+  for (var i = 0; i < keys.length; i++) {
+    var name = keys[i];
+    for (var j = 0; j < nameObj[name].length; j++) {
+      debugger;
+      var emails = Object.keys(nameObj[name][j]).sort();
+
+      var fullUser = [name];
+      fullUser = fullUser.concat(emails);
+      result.push(fullUser);
+    }
+  }
+  return result;
 };
+
+var accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]];
+
+console.log(accountsMerge(accounts));
---
+++ Not Completed Leetcode Problems/subarraySum.js
@@ -0,0 +1,3 @@
+var subarraySum = function(nums, k) {
+
+};
