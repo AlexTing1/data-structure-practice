77aaa43d55f2a69370aeb226c185c1d22b620c83
Alex Ting <alexting6366@gmail.com>
2021-09-06 13:09:02 -0700

    completed isRobotBounded.js

--- Not Completed Leetcode Problems/isRobotBounded.js
+++ Not Completed Leetcode Problems/isRobotBounded.js
@@ -1,40 +1,32 @@
 var isRobotBounded = function(instructions) {
-  var history = {};
   var direction = [[0, 1], [1, 0], [0, -1], [-1, 0]];
   var directionIndex = 0;
-  var currentLocation = [0, 0];
-  history[currentLocation] = 1;
+  var x = 0;
+  var y = 0;
   for (var i = 0; i < instructions.length; i++) {
     var currentInstruction = instructions[i];
     debugger;
     if (currentInstruction === 'G') {
-      currentLocation[0] += direction[directionIndex][0];
-      currentLocation[1] += direction[directionIndex][1];
+      x += direction[directionIndex][0];
+      y += direction[directionIndex][1];
       /* if (history[currentLocation] !== undefined) {
         return true;
       } else {
         history[currentLocation] = 1;
       } */
     } else if (currentInstruction === 'R') {
-      if (directionIndex + 1 >= direction.length) {
-        directionIndex = 0;
-      } else {
-        directionIndex++;
-      }
+      directionIndex = (directionIndex + 3) % 4;
     } else {
-      if (directionIndex - 1 <= 0) {
-        directionIndex = direction.length - 1;
-      } else {
-        directionIndex--;
-      }
+      directionIndex = (directionIndex + 1) % 4;
     }
   }
-  if (directionIndex !== 0 || currentLocation === [0, 0]) {
+  if (directionIndex !== 0 || (x === 0 && y === 0)) {
     return true;
   } else {
     return false;
   }
 };
 
-var instructions = "GGLLGG";
+var instructions = "GLGLGGLGL";
 console.log(isRobotBounded(instructions));
+
