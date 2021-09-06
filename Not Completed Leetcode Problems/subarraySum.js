8a23c5300bd86733c8fa7fdc49043e717adf1b43
Alex Ting <alexting6366@gmail.com>
2021-09-05 18:27:03 -0700

    finished subarraySum.js

--- Not Completed Leetcode Problems/subarraySum.js
+++ Not Completed Leetcode Problems/subarraySum.js
@@ -1,3 +1,19 @@
 var subarraySum = function(nums, k) {
+  var count = 0;
+  var total = 0;
+  var prefixSumMap = {}; // {prefixSum: count}
+  prefixSumMap[0] = 1;
+  for (var i = 0; i < nums.length; i++) {
+    var current = nums[i];
+    total += current;
+    if (prefixSumMap.has(total - k)) {
+      count += prefixSumMap[total-k];
+    }
 
+    if (prefixSumMap[total] !== undefined) {
+      prefixSumMap[total]++;
+    } else {
+      prefixSumMap[total] = 1;
+    }
+  }
 };
