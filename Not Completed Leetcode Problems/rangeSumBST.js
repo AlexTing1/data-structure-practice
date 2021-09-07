3a9efe75e8d164b955a31aded9a5d4e5ddb9d5f1
Alex Ting <alexting6366@gmail.com>
2021-09-06 18:00:35 -0700

    created rangeSumBST.js

--- Not Completed Leetcode Problems/rangeSumBST.js
+++ Not Completed Leetcode Problems/rangeSumBST.js
@@ -1,3 +1,19 @@
 var rangeSumBST = function(root, low, high) {
+  var sum = 0;
 
+  var helper = function(currentNode) {
+      if (currentNode === null) {
+          return;
+      }
+
+      if (currentNode.val >= low && currentNode.val <= high) {
+          sum += currentNode.val;
+      }
+
+      helper(currentNode.left);
+      helper(currentNode.right);
+  }
+
+  helper(root);
+  return sum;
 };
