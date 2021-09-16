959995f5766b05420480f72006ad0d3b9e8f97c3
Alex Ting <alexting6366@gmail.com>
2021-09-16 11:31:08 -0700

    started countAndSay.js

--- Not Completed Leetcode Problems/maxSlidingWindow.js
+++ Not Completed Leetcode Problems/maxSlidingWindow.js
@@ -1,3 +1,9 @@
 var maxSlidingWindow = function(nums, k) {
-
+  var result = [];
+  for (var i = 0; i + k < nums.length; i++) {
+    var currentBlock = nums.slice(i, i + k);
+    var max = Math.max(...currentBlock);
+    result.push(max);
+  }
+  return result;
 };
