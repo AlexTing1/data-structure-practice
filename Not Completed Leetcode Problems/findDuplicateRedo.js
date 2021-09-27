2421b640a9030a82ac7debc3fdb26749785d21cf
Alex Ting <alexting6366@gmail.com>
2021-09-27 14:24:49 -0700

    completed duplicateFileSystem redo

--- Not Completed Leetcode Problems/findDuplicateRedo.js
+++ Not Completed Leetcode Problems/findDuplicateRedo.js
@@ -1,3 +1,31 @@
 var findDuplicate = function(paths) {
+  var tracker = {};
+  var result = [];
+  for (var i = 0; i < paths.length; i++) {
+    var current = paths[i].split(' ');
+    var currentFilePath = current[0];
+    for (var j = 1; j < current.length; j++) {
+      //  ex. 1.txt(abcd);
+      var currentFile = current[j];
+      var paraIndex = currentFile.indexOf('(');
+      var content = currentFile.slice(paraIndex);
+      var fileName = currentFile.slice(0, paraIndex);
+      var combinedPathContent = currentFilePath + '/' + fileName;
+      if (tracker[content] === undefined) {
+        tracker[content] = [combinedPathContent];
+      } else {
+        tracker[content].push(combinedPathContent);
+      }
+    }
+  }
 
+  var keys = Object.keys(tracker);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (tracker[key].length > 1) {
+      result.push(tracker[key]);
+    }
+
+  }
+  return result;
 };
