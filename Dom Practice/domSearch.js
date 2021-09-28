a59fc8969335845c94f1094d126a1be5fb7544ab
Alex Ting <alexting6366@gmail.com>
2021-09-27 21:47:48 -0700

    completed getByClassNameBFS function

--- Dom Practice/domSearch.js
+++ Dom Practice/domSearch.js
@@ -19,7 +19,23 @@ function getByClassName(root, className) {
 }
 
 function getByClassNameBFS(root, className) {
+  //todo
+  var result = [];
+  var queue = [root];
 
+  while (queue.length !== 0) {
+    var currentNode = queue.shift();
+    if (currentNode.className === className) {
+      result.push(currentNode);
+    }
+
+    for (var i = 0; i < currentNode.children.length; i++) {
+      var child = currentNode.children[i];
+      queue.push(child);
+    }
+  }
+
+  return result;
 }
 
 const { JSDOM } = require("jsdom");
@@ -39,14 +55,14 @@ const { document } = new JSDOM(`
 `).window;
 const getIds = (elements=[]) => Array.from(elements).map(x => x.id);
 const root = document.getElementById('root');
-console.log('actual:  ', getIds(getByClassName(root, 'a')));
+console.log('actual:  ', getIds(getByClassNameBFS(root, 'a')));
 console.log('expected:' , `[ 'root', 'a-2', 'a-3' ]`);
 
-console.log('actual:  ', getIds(getByClassName(root, 'b')));
+console.log('actual:  ', getIds(getByClassNameBFS(root, 'b')));
 console.log('expected:' , `['b-1']`);
 
-console.log('actual:  ', getIds(getByClassName(root, 'c')));
+console.log('actual:  ', getIds(getByClassNameBFS(root, 'c')));
 console.log('expected:' , `['c-1']`);
 
-console.log('actual:  ', getIds(getByClassName(root, 'd')));
+console.log('actual:  ', getIds(getByClassNameBFS(root, 'd')));
 console.log('expected:' , `['d-1', 'd-2']`);
