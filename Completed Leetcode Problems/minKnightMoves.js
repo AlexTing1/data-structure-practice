1fed3b66e455f0cf6d38535026d6cceb88397fc1
AlexTing1 <alexting6366@gmail.com>
2021-11-11 14:41:07 -0800

    test

--- Completed Leetcode Problems/minKnightMoves.js
+++ Completed Leetcode Problems/minKnightMoves.js
@@ -2,37 +2,37 @@ var minKnightMoves = function(x, y) {
   x = Math.abs(x);
   y = Math.abs(y);
 
-  let tracker = {};
-  let count = 0;
   let queue = [[0, 0]];
+  let count = 0;
+  let tracker = {};
 
   const xDir = [-2, -2, -1, -1, 1, 1, 2, 2];
-  const yDir = [1, -1, 2, -2, 2, -2, 1, -1];
+  const yDir = [1, -1, -2, 2, 2, -2, 1, -1];
 
   while (queue) {
-
     const queueLength = queue.length;
 
     for (var i = 0; i < queueLength; i++) {
-      let current = queue.shift();
-      if (current[0] === x && current[1] === y) {
-        return count;
-      }
+      const current = queue.shift();
       for (var j = 0; j < xDir.length; j++) {
-        const xValue = current[0] + xDir[j];
-        const yValue = current[1] + yDir[j];
-        const coord = [xValue, yValue];
+        debugger;
 
-        if (tracker[coord] === undefined && xValue >= -2 && yValue >= -2) {
-          queue.push(coord);
-          tracker[coord] = 1;
+        if (current[0] === x && current[1] === y) {
+          return count;
         }
-      }
+        const newX = current[0] + xDir[j];
+        const newY = current[1] + yDir[j];
 
+        if (tracker[[newX, newY]] === undefined && newX >= -2 && newY >= -2) {
+          queue.push([newX, newY]);
+          tracker[[newX, newY]] = 1;
+        }
+
+      }
     }
     count++;
   }
-  return -1;
+
 };
 
 console.log(minKnightMoves(5, 5));
